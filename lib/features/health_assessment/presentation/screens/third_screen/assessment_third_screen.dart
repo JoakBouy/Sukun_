052d0558ec26b37e9f7faf4aba65dac64e5c778d
Moaz Talaat <88838071+MoazSalem@users.noreply.github.com>
2025-06-18 19:44:32 +0300

    Assessment Screen - Third Screen: completed

--- lib/features/health_assessment/presentation/screens/third_screen/assessment_third_screen.dart
+++ lib/features/health_assessment/presentation/screens/third_screen/assessment_third_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:freud_ai/core/managers/sizes_manager.dart';
+import 'package:freud_ai/core/managers/strings_manager.dart';
+import 'package:freud_ai/features/health_assessment/presentation/screens/third_screen/widgets/age_widget.dart';
 
 class AssessmentThirdScreen extends StatefulWidget {
   const AssessmentThirdScreen({super.key});
@@ -9,11 +11,32 @@ class AssessmentThirdScreen extends StatefulWidget {
 }
 
 class _AssessmentThirdScreenState extends State<AssessmentThirdScreen> {
+  final CarouselController _controller = CarouselController(initialItem: 17);
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(SizesManager.padding),
-      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: []),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(SizesManager.padding),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              textAlign: TextAlign.center,
+              StringsManager.assessmentTitle3,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            SizedBox(height: SizesManager.dhPadding),
+            AgeWidget(controller: _controller),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/features/health_assessment/presentation/screens/third_screen/widgets/age_widget.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:freud_ai/core/managers/colors_manager.dart';
+
+class AgeWidget extends StatelessWidget {
+  const AgeWidget({super.key, required this.controller});
+  final CarouselController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 450,
+      child: CarouselView.weighted(
+        controller: controller,
+        scrollDirection: Axis.vertical,
+        flexWeights: [2, 3, 7, 3, 2],
+        itemSnapping: true,
+        backgroundColor: Colors.transparent,
+        enableSplash: false,
+        children: List<Widget>.generate(
+          100,
+          (index) => Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                return constraints.maxHeight > 108
+                    ? Card(
+                      margin: const EdgeInsets.symmetric(horizontal: 45),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(200),
+                        side: BorderSide(
+                          color: ColorsManager.green.withAlpha(80),
+                          width: 4,
+                          strokeAlign: BorderSide.strokeAlignOutside,
+                        ),
+                      ),
+                      elevation: 0,
+                      color: ColorsManager.green,
+                      child: Center(
+                        child: Text(
+                          (index + 1).toString(),
+                          style: TextStyle(
+                            fontSize: 130,
+                            fontWeight: FontWeight.w900,
+                            height: 0.6,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    )
+                    : Transform.scale(
+                      scale: 1.4,
+                      child: Center(
+                        child: Text(
+                          (index + 1).toString(),
+                          style: TextStyle(
+                            fontSize: (constraints.maxHeight / 1.2).clamp(
+                              28,
+                              85,
+                            ),
+                            fontWeight: FontWeight.w900,
+                            height: 0.8,
+                            color: Theme.of(
+                              context,
+                            ).colorScheme.secondary.withAlpha(
+                              (constraints.maxHeight / 0.6).toInt().clamp(
+                                0,
+                                255,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
